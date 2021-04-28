fae13e20e011465ad58df67d120ad43cc634e135
alexdred <oleksii.dred@gmail.com>
2021-04-28 02:01:46 +0200

    index and registration done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict';
 
+$('.header__menu-button').click(function () {
+    $('.header__navigation').css('display', 'block');
+});
+
+$('.header__navigation-close').click(function () {
+    $('.header__navigation').css('display', 'none');
+});
