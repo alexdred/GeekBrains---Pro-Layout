8777af216a632bbb07bf1671fdb195237a73a65e
alexdred <oleksii.dred@gmail.com>
2021-06-01 23:22:18 +0200

    json render

--- js/js_pro_hw.js
+++ js/js_pro_hw.js
@@ -1,52 +1,51 @@
 'use strict';
 
-class Product {
-    constructor(id, title, price, image) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.image = image;
-    }
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+class Products {
+    constructor(container = '.products__list') {
+        this.container = container;
+        this.goods = [];
+        this._getProducts()
+            .then(goods => {
+                this.goods = [...goods];
+                this.render();
+                this.totalPrice();
+            });
+    };
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(list => list.json())
+    };
 
     renderProduct = product => {
         return `<li class="products__item">
                 <div class="products__item-wrap">
-                    <img class="products__item-image" src="./img/js_pro_hw/${product.image}" alt="${product.title}" width="360"
+                    <img class="products__item-image" src="./img/js_pro_hw/laptop.jpg" alt="${product.product_name}" width="360"
                         height="420">
                     <button class="products__item-button" type="button">
                         <img class="products__item-button-img" src="./img/main/cart.svg" alt="cart" width="26"
                             height="24">Add to Cart</button>
                 </div>
-                <h3 class="products__item-heading"><a href="product.html">${product.title}</a></h3>
+                <h3 class="products__item-heading"><a href="product.html">${product.product_name}</a></h3>
                 <p class="products__item-price">$${product.price}</p>
             </li>`
-    };
+    }
 
-    renderPage = list => {
-        const productsList = list.map(item => this.renderProduct(item));
-        document.querySelector('.products__list-home').innerHTML += productsList.join("");
+    render() {
+        for (let i = 0; i < this.goods.length; i++) {
+            document.querySelector(this.container).insertAdjacentHTML('beforeend', this.renderProduct(this.goods[i]));
+        }
     };
 
     totalPrice() {
         let total = 0;
-        products.forEach(product => {
-            total += product.price;
-        });
-        console.log(total);
-        document.querySelector('.total-price').innerHTML = total;
+        for (let i = 0; i < this.goods.length; i++) {
+            total += this.goods[i].price;
+            document.querySelector(".total-price").innerHTML = total;
+        }
     }
 }
 
-// products
-const products = [
-    new Product(1, "ellery x m'o capsule", 51, "product-1.jpeg"),
-    new Product(2, "ellery x m'o capsule", 52, "product-1.jpeg"),
-    new Product(3, "ellery x m'o capsule", 53, "product-1.jpeg"),
-    new Product(4, "ellery x m'o capsule", 54, "product-1.jpeg"),
-    new Product(5, "ellery x m'o capsule", 55, "product-1.jpeg"),
-    new Product(6, "ellery x m'o capsule", 56, "product-1.jpeg"),
-];
-
-const food = new Product();
-food.renderPage(products);
-food.totalPrice();
+let tech = new Products();
